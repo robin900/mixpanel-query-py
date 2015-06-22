e3691cce079ea5f12e9d9e4f0a2b78bed9bb5301
Robin Thomas <rthomas900@gmail.com>
2015-06-22 14:38:18 -0400

    use correct event names to match seeded events

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -11,12 +11,12 @@ class SmokeTestCase(unittest.TestCase):
         self.client = mixpanel_query.client.MixpanelQueryClient(self._key, self._secret)
 
     def test_get_events(self):
-        data = self.client.get_events(['Some Event'], 'day', 10)
+        data = self.client.get_events(['event1'], 'day', 10)
 
     def test_get_event_properties_values(self):
-        data = self.client.get_event_properties_values(['Some Event'], 'some_property')
+        data = self.client.get_event_properties_values(['event1'], 'foo')
 
     def test_get_export(self):
-        data = self.client.get_export('2015-06-22', '2015-06-22', ['Some Event'])
+        data = self.client.get_export('2015-06-22', '2015-06-22', ['event1'])
         next(data)
 
